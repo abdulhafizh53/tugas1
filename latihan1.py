523c35e0a791b68dcf538ae6eef583edc9c982fc
abdulhafizh53 <apisjr53@gmail.com>
2023-03-03 15:18:42 +0700

    commit v1.2

--- latihan1.py
+++ latihan1.py
@@ -2,8 +2,15 @@
     #print(i)
 
 
-for i in range(1, 101):
+'''for i in range(1, 101):
   if(i%2 == 0):
     continue
   else:
-    print(i)
+    print(i)'''
+
+def is_odd(num):
+    return num % 2 == 1
+
+for i in range(1, 101):
+    if is_odd(i):
+        print(i)
